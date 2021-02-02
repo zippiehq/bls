cefc91b285597200ff5e51f9ac91136bf9727ab5
Carsten Munk <carsten.munk@zippie.org>
2021-02-02 22:17:10 +0100

    Change example to take serialized signatures
    Signed-off-by: Carsten Munk <carsten.munk@zippie.org>

--- sample/bls12_381_smpl.cpp
+++ sample/bls12_381_smpl.cpp
@@ -21,11 +21,19 @@ int main()
 	blsSecretKeySetByCSPRNG(&blinding);
 	blsGetPublicKey(&pub, &sec);
 
+	char sig_[48], sig_blinded_[48], sig_unblinded_[48];
+	/// BLS_DLL_API void blsBlindSignatureSign(blsSignature *sig, const blsSecretKey *sec, int inverse, const void *m, mclSize size);
+
 	blsSign(&sig_blinded, &blinding, msg, msgSize);
-	blsBlindSignatureSign(&sig_blinded, &sig, &sec, 0);
-	blsBlindSignatureSign(&sig, &sig_unblinded, &blinding, 1);
+	printf("%i\n", blsSignatureSerialize(sig_blinded_, 48, &sig_blinded));
+
+	blsBlindSignatureSign(&sig, &sec, 0, sig_blinded_, 48);
+
+	printf("%i\n", blsSignatureSerialize(sig_, 48, &sig));
+	
+	blsBlindSignatureSign(&sig_unblinded, &blinding, 1, sig_, 48);
+	
 	// BLS_DLL_API int blsVerify(const blsSignature *sig, const blsPublicKey *pub, const void *m, mclSize size);
 	printf("%i\n", blsVerify(&sig_unblinded, &pub, msg, msgSize));
 
-	// BLS_DLL_API void blsBlindSignatureSign(blsSignature *orig_sig, blsSignature *sig, const blsSecretKey *sec, int inverse) {
 }
